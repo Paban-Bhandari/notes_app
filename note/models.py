3c15ea8611894fce84f537148ef3a7216dccc4e6
Paban-Bhandari <bhandaripawan841@gmail.com>
2026-07-31 15:57:01 +0545

    Created a note model

--- note/models.py
+++ note/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
